bba32711c0ce8c2b9e781b91a9b03b485b0cb611
WON SEOB SEO <flyjwayur@gmail.com>
2017-01-14 19:29:32 +0200

    Make phzr 'Hello world'

--- src/puzzle/core.cljs
+++ src/puzzle/core.cljs
@@ -1,9 +1,36 @@
 (ns puzzle.core
-  (:require []))
+  (:require [phzr.core :as p]
+            [phzr.game :as pg]
+            [phzr.game-object-factory :as pgof]
+            [phzr.loader :as pl]))
 
 (enable-console-print!)
 
 ;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce game-state (atom {:text         "Hello world!"
+                           :world-width  800
+                           :world-height 600
+                           :game-div-id  "puzzle-game"}))
 
-(println (:text @app-state))
+(println (:text @game-state))
+
+(defn ^:private preload
+  [game]
+  (let [loader (:load game)]
+    (doto loader
+      (pl/image "logo" "images/phaser.png"))))
+
+(defn ^:private create
+  [game]
+  (let [game-object-factory (:add game)
+        sprite (pgof/sprite game-object-factory 300 200 "logo")]))
+
+(def build-states
+  {:preload preload
+   :create  create})
+
+(pg/->Game (:world-width @game-state)
+           (:world-height @game-state)
+           (p/phaser-constants :auto)
+           (:game-div-id @game-state)
+           build-states)
