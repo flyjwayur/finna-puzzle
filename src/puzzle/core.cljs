ae9c2d5cf2597ef302dad7de1c97a841062bb929
HyeSoo <flyjwayur@users.noreply.github.com>
2018-06-16 16:02:53 +0300

    Print image url

--- src/puzzle/core.cljs
+++ src/puzzle/core.cljs
@@ -6,7 +6,12 @@
 
 (go (let [response (<! (http/get "https://api.finna.fi/v1/search"
                                  {:with-credentials? false
-                                  :query-params {"lookfor" "kirkko"}}))]
+                                  :query-params {"lookfor" "kirkko"}}))
+          _ (-> (filter #(not (nil? (:images %))) (get-in response [:body :records]))
+                              first
+                              :images
+                              first
+                              println)]
       (cljs.pprint/pprint (-> (filter #(not (nil? (:images %))) (get-in response [:body :records]))
                               first
                               :images
